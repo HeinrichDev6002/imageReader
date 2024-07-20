f80ffc945d840973006884b3c3e4806e78e8b422
HeinrichDev6002 <tutorgames.gmil.com@gmail.com>
2024-07-20 14:49:48 -0300

    feat: implementar leitura e escrita de imagem na interface

--- src/MenuClass.java
+++ src/MenuClass.java
@@ -1,10 +1,20 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MenuClass {
     JMenuBar menuBar;
     JMenu menu, menuLanguage, menuColor;
     JMenuItem iOpen, iSave, iSaveAs, iColorDarkGray, iColorBlack, iColorWhite, iLanguageENG, iLanguagePT;
     MyWindow myWindow;
+    String fileName, fileAdress;
+    BufferedImage img = null;
+    JLabel image;
    public MenuClass(MyWindow myWindow){
    this.myWindow = myWindow;
 
@@ -23,12 +33,55 @@ public class MenuClass {
        iLanguagePT = new JMenuItem("Português");
 
        menu = new JMenu("File");
-       menuLanguage = new JMenu("Language");
+       menu.add(iOpen);
+       menu.add(iSave);
+       menu.add(iSaveAs);
        menuColor = new JMenu("Color");
+        menuColor.add(iColorDarkGray);
+        menuColor.add(iColorBlack);
+        menuColor.add(iColorWhite);
+        menuLanguage = new JMenu("Language");
+        menuLanguage.add(iLanguageENG);
+        menuLanguage.add(iLanguagePT);
+
+        iOpen.addActionListener(myWindow);
+        iOpen.setActionCommand("Open");
+
+
+        menuBar.add(menu);
+        menuBar.add(menuColor);
+        menuBar.add(menuLanguage);
+
 
 
 
 
     }
+    public void open(){
+        FileDialog fileDialog = new FileDialog(myWindow.frame,"Files",FileDialog.LOAD);
+        fileDialog.setVisible(true);
+        if(fileDialog.getFile()!=null) {
+            fileAdress = fileDialog.getDirectory();
+            fileName = fileDialog.getFile();
+
+
+        }
+        try{
+            img = ImageIO.read(new File(fileAdress + fileName));
+            System.out.println(img);
+            ImageIcon imageIcon = new ImageIcon(img);
+            JLabel label = new JLabel();
+            label.setIcon(imageIcon);
+            myWindow.frame.add(label);
+            label.setBounds(200,200,myWindow.frame.getWidth(), myWindow.frame.getHeight());
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+   }
+
 
 }
--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyWindow {
+public class MyWindow implements ActionListener {
     JFrame frame;
     JMenuBar menuBar;
     JMenu menu;
     JMenuItem menuItem;
+    JPanel panel;
     MenuClass myMenu = new MenuClass(this);
     MyWindow(){
         createFrame();
+        myMenu.createMenu();
         frame.setVisible(true);
     }
     private void createFrame(){
@@ -18,6 +22,17 @@ public class MyWindow {
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout(10,20));
+        //frame.add(panel, BorderLayout.CENTER);
 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+String command = e.getActionCommand();
+switch (command){
+    case "Open":
+myMenu.open();
+}
     }
 }
